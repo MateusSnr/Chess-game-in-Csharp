b64d5cdf1648fbef3d57bc1fddc77fda48d73724
“Mateus <devmateussnr@gmail.com>
2023-06-03 13:19:20 -0300

    Comecando a colocar pecas no tabuleiro

---
+++ xadrez_console/xadrez/Rei.cs
@@ -0,0 +1,16 @@
+﻿using tabuleiro;
+namespace xadrez
+{
+    class Rei : Peca
+    {
+        public Rei (Tabuleiro tab, Cor cor ) : base (tab, cor)
+        {
+            //Criando um objeto rei, utilizando a superclasse peca
+        }
+
+        public override string ToString()
+        {
+            return "R";
+        }
+    }
+}

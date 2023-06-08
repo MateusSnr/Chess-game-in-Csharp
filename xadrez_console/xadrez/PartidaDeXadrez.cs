7fb689f04e55da56b888bde2dbf93bf1eead83b6
“Mateus <devmateussnr@gmail.com>
2023-06-08 20:42:11 -0300

    jogada especial en passant

--- xadrez_console/xadrez/PartidaDeXadrez.cs
+++ xadrez_console/xadrez/PartidaDeXadrez.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Common;
 using System.Runtime.Serialization.Formatters;
 using tabuleiro;
 using xadrez;
@@ -15,6 +16,7 @@ namespace xadrez
         private HashSet<Peca> pecas;//Coleção de dados.Conjunto que vai guardar todas as peças.
         private HashSet<Peca> capturadas;
         public bool xeque { get; private set; }
+        public Peca vulneravelEnPassant { get; private set; }
 
         public PartidaDeXadrez()
         {
@@ -23,6 +25,7 @@ namespace xadrez
             jogadorAtual = Cor.Branca;
             pecas = new HashSet<Peca>();
             capturadas = new HashSet<Peca>();
+            vulneravelEnPassant = null;
             colocarPecas();
             terminada = false;
             xeque = false;
@@ -60,6 +63,26 @@ namespace xadrez
                 t.incrementarQtdMovimentos();
                 tab.colocarPeca(t, destinoTorre);
             }
+
+            //#jogadaespecial en passant 
+            if(p is Peao)
+            {
+                if(origem.coluna != destino.coluna && pecaCapturada == null) 
+                {
+                    Posicao posPeao;
+                    if(p.cor == Cor.Branca)
+                    {
+                        posPeao = new Posicao(destino.linha + 1, destino.coluna);
+                    }
+                    else
+                    {
+                        posPeao = new Posicao(destino.linha - 1, destino.coluna);
+                    }
+                    pecaCapturada = tab.retirarPeca(posPeao);
+                    capturadas.Add(pecaCapturada);
+                }
+            }
+
             return pecaCapturada;
         }
 
@@ -87,6 +110,18 @@ namespace xadrez
             {
                 turno++;
                 mudaJogador();
+            }
+
+            Peca p = tab.peca(destino);
+
+            //#jogadaespecial en passant
+            if(p is Peao && (destino.linha == origem.linha-2 || destino.linha ==origem.linha+2)) 
+            {
+                vulneravelEnPassant = p;
+            }
+            else
+            {
+                vulneravelEnPassant = null;
             }
         }
 
@@ -121,6 +156,25 @@ namespace xadrez
                 Peca t = tab.retirarPeca(destinoTorre);
                 t.decrementarQtdMovimentos();
                 tab.colocarPeca(t, origemTorre);
+            }
+
+            //#jogadaespecial en passant 
+            if(p is Peao)
+            {
+                if(origem.coluna != destino.coluna && pecaCapturada == vulneravelEnPassant)
+                {
+                    Peca peao = tab.retirarPeca(destino);
+                    Posicao posPeao;
+                    if(p.cor == Cor.Branca)
+                    {
+                        posPeao = new Posicao (3,destino.coluna);
+                    }
+                    else
+                    {
+                        posPeao = new Posicao(4, destino.coluna);
+                    }
+                    tab.colocarPeca(peao, posPeao);
+                }
             }
         }
 
@@ -253,14 +307,14 @@ namespace xadrez
             colocarNovaPeca('g', 1, new Bispo(tab, Cor.Branca));
             colocarNovaPeca('f', 1, new Cavalo(tab, Cor.Branca));
             colocarNovaPeca('h', 1, new Torre(tab, Cor.Branca));
-            colocarNovaPeca('a', 2, new Peao(tab, Cor.Branca));
-            colocarNovaPeca('b', 2, new Peao(tab, Cor.Branca));
-            colocarNovaPeca('c', 2, new Peao(tab, Cor.Branca));
-            colocarNovaPeca('d', 2, new Peao(tab, Cor.Branca));
-            colocarNovaPeca('e', 2, new Peao(tab, Cor.Branca));
-            colocarNovaPeca('f', 2, new Peao(tab, Cor.Branca));
-            colocarNovaPeca('g', 2, new Peao(tab, Cor.Branca));
-            colocarNovaPeca('h', 2, new Peao(tab, Cor.Branca));
+            colocarNovaPeca('a', 2, new Peao(tab, Cor.Branca,this));
+            colocarNovaPeca('b', 2, new Peao(tab, Cor.Branca,this));
+            colocarNovaPeca('c', 2, new Peao(tab, Cor.Branca, this));
+            colocarNovaPeca('d', 2, new Peao(tab, Cor.Branca, this));
+            colocarNovaPeca('e', 2, new Peao(tab, Cor.Branca, this));
+            colocarNovaPeca('f', 2, new Peao(tab, Cor.Branca, this));
+            colocarNovaPeca('g', 2, new Peao(tab, Cor.Branca, this));
+            colocarNovaPeca('h', 2, new Peao(tab, Cor.Branca, this));
 
             colocarNovaPeca('a', 8, new Torre(tab, Cor.Preta));
             colocarNovaPeca('b', 8, new Cavalo(tab, Cor.Preta));
@@ -270,14 +324,14 @@ namespace xadrez
             colocarNovaPeca('g', 8, new Bispo(tab, Cor.Preta));
             colocarNovaPeca('f', 8, new Cavalo(tab, Cor.Preta));
             colocarNovaPeca('h', 8, new Torre(tab, Cor.Preta));
-            colocarNovaPeca('a', 7, new Peao(tab, Cor.Preta));
-            colocarNovaPeca('b', 7, new Peao(tab, Cor.Preta));
-            colocarNovaPeca('c', 7, new Peao(tab, Cor.Preta));
-            colocarNovaPeca('d', 7, new Peao(tab, Cor.Preta));
-            colocarNovaPeca('e', 7, new Peao(tab, Cor.Preta));
-            colocarNovaPeca('f', 7, new Peao(tab, Cor.Preta));
-            colocarNovaPeca('g', 7, new Peao(tab, Cor.Preta));
-            colocarNovaPeca('h', 7, new Peao(tab, Cor.Preta));
+            colocarNovaPeca('a', 7, new Peao(tab, Cor.Preta, this));
+            colocarNovaPeca('b', 7, new Peao(tab, Cor.Preta, this));
+            colocarNovaPeca('c', 7, new Peao(tab, Cor.Preta, this));
+            colocarNovaPeca('d', 7, new Peao(tab, Cor.Preta, this));
+            colocarNovaPeca('e', 7, new Peao(tab, Cor.Preta, this));
+            colocarNovaPeca('f', 7, new Peao(tab, Cor.Preta, this));
+            colocarNovaPeca('g', 7, new Peao(tab, Cor.Preta, this));
+            colocarNovaPeca('h', 7, new Peao(tab, Cor.Preta, this));
         }
 
         public void validarPosicaoDeOrigem(Posicao pos)
